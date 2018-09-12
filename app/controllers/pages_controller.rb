46b27313517dfa095aa05290c509cdc279c8bdfb
Henrique Silva <hbs003@hotmail.com>
2018-09-12 18:26:18 -0300

    first

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
+  end
+
+  def contato
+  end
+
+  def services
   end
 end
